6822eb46f1bea7b56f5d2ad9988d394638a39114
Maksym-Holovachuk <mgolovachuk@gmail.com>
2024-03-13 20:03:22 +0200

    added setup for dark/light themes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between">
-      <div>Hello new project!</div>
+      <h1>Hello new project!</h1>
     </main>
   );
 }
